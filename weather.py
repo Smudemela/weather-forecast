1f487d38da2160560e66ef13478ee0a59beae77a
Sridatta <gautham@Gauthams-MacBook-Pro.local>
2026-05-14 16:25:19 -0700

    fetch and display weather data

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ url = "https://api.open-meteo.com/v1/forecast"
 
 params = {
 	"latitude": 37.73,
-	"logitude": -121.0,
+	"longitude": -121.0,
 	"hourly": ["temperature_2m", "precipitation", "windspeed_10m"],
 	"past_days": 7,
 	"forecast_days": 7
@@ -13,4 +13,10 @@ params = {
 
 response = requests.get(url, params=params)
 
-data = response.json
+data = response.json()
+
+df = pd.DataFrame(data["hourly"])
+df["time"] = pd.to_datetime(df["time"])
+
+
+print(df)
